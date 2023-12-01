2c05f9fe1123ff5485d80f26afa4549aa2fe0dd6
Lorenzo Arcioni <lorenzo.arcioni2000@gmail.com>
2023-12-01 18:35:48 +0100

    evvai

--- src/buscoplotpy/graphics/synteny.py
+++ src/buscoplotpy/graphics/synteny.py
@@ -5,7 +5,88 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from buscoplotpy.graphics.chromosome import Chromosome
 
-def synteny_plot(#df_1: pd.DataFrame, 
+CHR_DISTANCE = 2
+CHR_FACTOR = 90
+
+# Set the x and y limits
+X_lim = 180
+Y_lim = 100
+
+def plot_left_karyotype(karyotype: pd.DataFrame, dim: int, ax):
+
+    # Initialize the step
+    step = 0
+
+    # Get the chromosome length sum
+    chr_len_sum = karyotype['end'].sum()
+
+    # Plot the karyotypes
+    for index, row in karyotype.iterrows():
+
+        # Get the dimension of the karyotype
+        chr_dim = row['end']
+
+        # Define the coordinates for the rectangle
+        x_start = len(max(karyotype['chr'], key=len))
+        x_end   = x_start + dim
+
+        y_start = CHR_DISTANCE + step
+        y_end   = y_start + chr_dim * (CHR_FACTOR - CHR_DISTANCE * len(karyotype)) / chr_len_sum
+
+        step = y_end
+
+        # Get the color
+        color = 'gray'
+
+        C = Chromosome(x_start=x_start,
+                       x_end=x_end,
+                       y_start=y_start,
+                       y_end=y_end,
+                       horizontal=True
+        )
+
+        C.add_label(x=3, y=(y_start + y_end) / 2, text=row['chr'], ha='center', va='center')
+
+        C.plot(ax)
+
+def plot_right_karyotype(karyotype: pd.DataFrame, dim: int, ax):
+
+    # Initialize the step
+    step = 0
+
+    # Get the chromosome length sum
+    chr_len_sum = karyotype['end'].sum()
+
+    # Plot the karyotypes
+    for index, row in karyotype.iterrows():
+
+        # Get the dimension of the karyotype
+        chr_dim = row['end']
+
+        # Define the coordinates for the rectangle
+        x_start = X_lim - len(max(karyotype['chr'], key=len))
+        x_end   = x_start - dim
+
+        y_start = CHR_DISTANCE + step
+        y_end   = y_start + chr_dim * (CHR_FACTOR - CHR_DISTANCE * len(karyotype)) / chr_len_sum
+
+        step = y_end
+
+        # Get the color
+        color = 'gray'
+
+        C = Chromosome(x_start=x_start,
+                       x_end=x_end,
+                       y_start=y_start,
+                       y_end=y_end,
+                       horizontal=True
+        )
+
+        C.add_label(x=X_lim - 3, y=(y_start + y_end) / 2, text=row['chr'], ha='center', va='center')
+
+        C.plot(ax)
+
+def vertical_synteny_plot(#df_1: pd.DataFrame, 
                  #df_2: pd.DataFrame,
                  karyotype_1: pd.DataFrame,
                  karyotype_2: pd.DataFrame,
@@ -56,22 +137,13 @@ def synteny_plot(#df_1: pd.DataFrame,
             return selected[1]
         else:
             return selected[2]
-        
-    # Create the synteny diagram
-    #fig, ax = plt.subplots()
-
-    # Read the karyotype file into a DataFrame
-    karyotype = pd.concat([karyotype_1, karyotype_2], ignore_index=True)
 
     # Lowercase the column names
-    karyotype.columns = karyotype.columns.str.lower()
-
-    # Set the x and y limits
-    X_lim = 100
-    Y_lim = 10 + 280 + 10
+    karyotype_1.columns = karyotype_1.columns.str.lower()
+    karyotype_2.columns = karyotype_2.columns.str.lower()
 
     # Create a new figure and axis
-    fig, ax = plt.subplots(figsize=(20, 20*Y_lim/X_lim), dpi=dpi)
+    fig, ax = plt.subplots(figsize=(18, 10), dpi=dpi)
 
     # Axis off
     ax.axis('off')
@@ -81,46 +153,8 @@ def synteny_plot(#df_1: pd.DataFrame,
     ax.set_xlim([0, X_lim])
 
     # Insert the plot title
-    ax.text(X_lim / 2, Y_lim - 1, title, fontsize=20, ha='center')
+    ax.text(X_lim / 2, Y_lim - 3, title, fontsize=20, ha='center')
 
-    # Calculate the maximum length of the chromosome name
-    chr_max_len = len(max(karyotype['chr'], key=len))
-
-    # Get the maximum length of the chromosome
-    chr_max_dim = karyotype['end'].max()
-
-    step = 0
-
-    # Plot the karyotypes
-    for index, row in karyotype.iterrows():
-
-        # Get the dimension of the karyotype
-        chr_dim = row['end']
-
-        # Define the coordinates for the rectangle
-        x_start = 30
-        x_end   = x_start + dim
-
-        y_start = 10 + step
-        y_end   = y_start + chr_dim * (Y_lim*9/10) / chr_max_dim
-
-        step += y_end
-
-        # Get the color
-        color = 'gray'
-
-        C = Chromosome(x_start=x_start,
-                       x_end=x_end,
-                       y_start=y_start,
-                       y_end=y_end,
-                       horizontal=True
-        )
-
-        C.add_label(x=(x_start + x_end) / 2, y=y_end - 5, text=row['chr'], ha='center', va='center')
-
-        C.plot(ax)
-
-    return karyotype
-
-    
+    plot_left_karyotype(karyotype_1, dim, ax)
+    plot_right_karyotype(karyotype_2, dim, ax)  
 
