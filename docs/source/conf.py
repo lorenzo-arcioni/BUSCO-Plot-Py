692f83a42a10c2f2277265e90212f14494c478be
Lorenzo Arcioni <lorenzo.arcioni2000@gmail.com>
2023-12-07 17:14:38 +0100

    ok

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,8 +23,8 @@ extensions = [
     'sphinx.ext.graphviz',
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
-    'substitutioncodeblock',
 ]
+
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -33,6 +33,5 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
